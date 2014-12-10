179dfc3563e42eb34199985f8b48851ab07a8a55
Jill Vogel <jill.vogel@adelaide.edu.au>
2014-12-10 15:52:46 +1030

    Changed HttpResponse mimetype parameter to content_type, as mimetype is deprecated.

--- database_files/views.py
+++ database_files/views.py
@@ -15,6 +15,6 @@ def serve(request, name):
         raise Http404('Filename is not an integer')
     f = get_object_or_404(File, pk=pk)
     mimetype = mimetypes.guess_type(name)[0] or 'application/octet-stream'
-    response = HttpResponse(base64.b64decode(f.content), mimetype=mimetype)
+    response = HttpResponse(base64.b64decode(f.content), content_type=mimetype)
     response['Content-Length'] = f.size
     return response
